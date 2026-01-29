619f621cd9520cc6adf212c00f928fd9d15c52f9
Cheston Leroy Ong <chestonleroy1@gmail.com>
2026-01-29 18:04:14 +0800

    Created transform component

--- Project Files/core/src/main/java/com/team6/arcadesim/components/TransformComponent.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/components/TransformComponent.java
@@ -3,4 +3,28 @@ package com.team6.arcadesim.components;
 import com.team6.arcadesim.ecs.Component;
 
 public class TransformComponent extends Component {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void update(float deltaTime) {
+        // Does nothing, the manager handles the transformation
+    }
+
+
 }
