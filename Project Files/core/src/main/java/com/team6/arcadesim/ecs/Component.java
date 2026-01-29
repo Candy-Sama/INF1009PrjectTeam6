36c058156a154a2bc7a3e31c1ab79f255b37ed53
Cheston Leroy Ong <chestonleroy1@gmail.com>
2026-01-29 17:49:13 +0800

    CollisionManager, MovementManager, Component Abstract completed

--- Project Files/core/src/main/java/com/team6/arcadesim/ecs/Component.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/ecs/Component.java
@@ -1,5 +1,15 @@
 package com.team6.arcadesim.ecs;
 
-public class Component {
+abstract public class Component {
+    private Entity owner;
 
+    public abstract void update(float deltaTime);
+
+    public Entity getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(Entity e) {
+        this.owner = e;
+    }
 }
