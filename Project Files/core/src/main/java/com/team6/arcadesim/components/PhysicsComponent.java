e266020111cc0792f2e787a9cce1c1862a18d18d
ch4nely <y33chan3l@gmail.com>
2026-01-31 21:18:00 +0800

    Update PhysicsComponent.java
    Edited PhysicsComponent coding

--- Project Files/core/src/main/java/com/team6/arcadesim/components/PhysicsComponent.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/components/PhysicsComponent.java
@@ -2,4 +2,32 @@ package com.team6.arcadesim.components;
 
 public class PhysicsComponent {
 
+    private float velocityX;
+    private float velocityY;
+
+    public PhysicsComponent() {
+        this.velocityX = 0.0f;
+        this.velocityY = 0.0f;
+    }
+
+    public PhysicsComponent(float velocityX, float velocityY) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
+
+    public float getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(float velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public float getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(float velocityY) {
+        this.velocityY = velocityY;
+    }
 }
