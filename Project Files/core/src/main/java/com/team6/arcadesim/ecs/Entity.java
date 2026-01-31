758548e965f54bca0118bed6ad88f06d86b5fbfc
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2026-01-31 21:46:11 +0800

    Editing Entity Manager

--- Project Files/core/src/main/java/com/team6/arcadesim/ecs/Entity.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/ecs/Entity.java
@@ -1,5 +1,31 @@
 package com.team6.arcadesim.ecs;
 
-public class Entity {
+import com.badlogic.gdx.graphics.Color;
+import java.util.List;
 
+public abstract class Entity {
+ 
+    private List<Component> listOfComponents;
+    private int id;
+    private boolean isActive;
+    
+    public void addComponent(Component component) {
+        listOfComponents.add(component);
+    }
+
+    //gets the component of the specified class type
+    public Component get(Class<? extends Component> componentClass) {
+        for (Component component : listOfComponents) {
+            if (component.getClass().equals(componentClass)) {
+                return component;
+            }
+        }
+        return null;
+    }
+
+    public void update(float delta) {
+        for (Component component : listOfComponents) {
+            component.update(delta);
+        }
+    }
 }
