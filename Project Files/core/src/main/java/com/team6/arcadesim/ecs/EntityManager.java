5bc318bce92dc23512cef7ceb3e225013b165979
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2026-01-31 21:46:11 +0800

    Editing Entity Manager

--- Project Files/core/src/main/java/com/team6/arcadesim/ecs/EntityManager.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/ecs/EntityManager.java
@@ -1,5 +1,12 @@
 package com.team6.arcadesim.ecs;
 
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+
 public class EntityManager {
 
+    private List<Entity> entityList;
+
+    public EntityManager() {
+     entityList = new List<Entity>();
+    }
 }
