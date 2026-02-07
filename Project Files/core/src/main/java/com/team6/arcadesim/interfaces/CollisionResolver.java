995365bc36c6453be4e2090160b48ebda47635bb
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2026-02-07 23:31:50 +0800

    Collision Update

--- Project Files/core/src/main/java/com/team6/arcadesim/interfaces/CollisionResolver.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/interfaces/CollisionResolver.java
@@ -3,5 +3,9 @@ package com.team6.arcadesim.interfaces;
 import com.team6.arcadesim.ecs.Entity;
 
 public interface CollisionResolver {
-    void resolveCollision(Entity a, Entity b);
+    /**
+     * Decodes the logic of what happens when two entities touch.
+     * This allows the engine to handle different game rules without code changes.
+     */
+    void resolve(Entity a, Entity b);
 }
--- Project Files/core/src/main/java/com/team6/arcadesim/managers/CollisionManager.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/managers/CollisionManager.java
@@ -2,34 +2,47 @@ package com.team6.arcadesim.managers;
 
 import java.util.List;
 
+import com.team6.arcadesim.components.CollisionComponent;
+import com.team6.arcadesim.components.TransformComponent;
 import com.team6.arcadesim.ecs.Entity;
 import com.team6.arcadesim.interfaces.CollisionResolver;
 
 public class CollisionManager {
+    private EntityManager entityManager;
     private CollisionResolver resolver;
-    private boolean isActive;
-
-    public CollisionManager() {
-
-    }
-
-    public void resolveCollision(List<Entity> entities) {
-
-    }
 
     public void setResolver(CollisionResolver resolver) {
-
+        this.resolver = resolver; // Strategy Pattern: Injecting the logic
     }
 
     public void update(float dt) {
+        // Filter entities that have both a position and a hitbox
+        List<Entity> collidables = entityManager.getEntitiesFor(CollisionComponent.class, TransformComponent.class);
 
+        for (int i = 0; i < collidables.size(); i++) {
+            for (int j = i + 1; j < collidables.size(); j++) {
+                Entity a = collidables.get(i);
+                Entity b = collidables.get(j);
+
+                if (checkCollision(a, b)) {
+                    if (resolver != null) {
+                        resolver.resolve(a, b); // Delegate the reaction to the resolver
+                    }
+                }
+            }
+        }
     }
 
-    public boolean isColliding(Entity a, Entity b) {
-        return false;
-    }
+    private boolean checkCollision(Entity a, Entity b) {
+        TransformComponent tA = a.getComponent(TransformComponent.class);
+        CollisionComponent cA = a.getComponent(CollisionComponent.class);
+        TransformComponent tB = b.getComponent(TransformComponent.class);
+        CollisionComponent cB = b.getComponent(CollisionComponent.class);
 
-    public void removeCollisionsInvolvingEntity(Entity e) {
-
+        // Standard AABB Collision Math [cite: 232, 236]
+        return tA.getPosition().x < tB.getPosition().x + cB.getWidth() &&
+               tA.getPosition().x + cA.getWidth() > tB.getPosition().x &&
+               tA.getPosition().y < tB.getPosition().y + cB.getHeight() &&
+               tA.getPosition().y + cA.getHeight() > tB.getPosition().y;
     }
 }
