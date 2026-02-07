d3e169672ec0e75401c09765017bc6a674218152
Cheston Leroy Ong <chestonleroy1@gmail.com>
2026-02-08 00:36:33 +0800

    abstractgamemaster edits

--- Project Files/core/src/main/java/com/team6/arcadesim/interfaces/CollisionResolver.java
+++ Project Files/core/src/main/java/com/team6/arcadesim/interfaces/CollisionResolver.java
@@ -3,9 +3,6 @@ package com.team6.arcadesim.interfaces;
 import com.team6.arcadesim.ecs.Entity;
 
 public interface CollisionResolver {
-    /**
-     * Decodes the logic of what happens when two entities touch.
-     * This allows the engine to handle different game rules without code changes.
-     */
+
     void resolveCollision(Entity a, Entity b);
 }
