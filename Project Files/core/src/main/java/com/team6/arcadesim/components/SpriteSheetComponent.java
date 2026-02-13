ca63ab68c2e8125416f3ce58e625f4d15b538701
Cheston Leroy Ong <chestonleroy1@gmail.com>
2026-02-13 12:08:38 +0800

    added SpriteSheet (future use), edited viewport manager

---
+++ Project Files/core/src/main/java/com/team6/arcadesim/components/SpriteSheetComponent.java
@@ -0,0 +1,48 @@
+package com.team6.arcadesim.components;
+
+import com.team6.arcadesim.ecs.Component;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class SpriteSheetComponent implements Component {
+
+    private TextureRegion region; // Better than Texture, supports sprite sheets
+    private float width;
+    private float height;
+    private boolean flipX;
+    private boolean flipY;
+
+    // Default constructor
+    public SpriteSheetComponent(Texture texture) {
+        this(texture, texture.getWidth(), texture.getHeight());
+    }
+
+    public SpriteSheetComponent(Texture texture, float width, float height) {
+        this.region = new TextureRegion(texture);
+        this.width = width;
+        this.height = height;
+        this.flipX = false;
+        this.flipY = false;
+    }
+
+    // --- Getters & Setters ---
+
+    public TextureRegion getRegion() { return region; }
+    
+    public void setTexture(Texture texture) {
+        this.region.setRegion(texture);
+    }
+
+    public float getWidth() { return width; }
+    public void setWidth(float width) { this.width = width; }
+
+    public float getHeight() { return height; }
+    public void setHeight(float height) { this.height = height; }
+
+    public boolean isFlipX() { return flipX; }
+    public void setFlipX(boolean flipX) { this.flipX = flipX; }
+
+    public boolean isFlipY() { return flipY; }
+    public void setFlipY(boolean flipY) { this.flipY = flipY; }
+}
