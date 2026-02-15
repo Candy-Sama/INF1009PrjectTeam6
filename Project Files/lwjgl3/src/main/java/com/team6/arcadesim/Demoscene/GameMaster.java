21cfb47a9dd7d4e5b6a43838a8e251b4484d61bc
Muhammad Mustaqim Bin Aziz <muspartan117@gmail.com>
2026-02-15 18:04:11 +0800

    Update GameMaster.java

--- Project Files/lwjgl3/src/main/java/com/team6/arcadesim/Demoscene/GameMaster.java
+++ Project Files/lwjgl3/src/main/java/com/team6/arcadesim/Demoscene/GameMaster.java
@@ -2,14 +2,14 @@ package com.team6.arcadesim.Demoscene;
 
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.team6.arcadesim.AbstractGameMaster;
-import com.team6.arcadesim.Demoscene.Scenes.DemoGravity;
+import com.team6.arcadesim.Demoscene.Scenes.DemoScene;
 
 public class GameMaster extends AbstractGameMaster {
 
     @Override
     public void init() {
         // Start the demo scene
-        sceneManager.setScene(new DemoGravity(this));
+        sceneManager.setScene(new DemoScene(this));
     }
 
     @Override
